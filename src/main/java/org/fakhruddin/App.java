821a6b220bd06cfe167d31f8e5a5797400f98549
Fakhruddin <muhammadfakhruddin@users.noreply.github.com>
2021-04-07 12:20:40 +0800

    Initial commit

---
+++ src/main/java/org/fakhruddin/App.java
@@ -0,0 +1,20 @@
+package org.fakhruddin;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App()
+    {
+        setTitle("Hello world");
+        setSize(600,400);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
